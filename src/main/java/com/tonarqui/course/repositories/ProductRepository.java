ca579230c71d060c9c86d70758d8dadf379590ae
alexandretonarqui <alexandre.tonarqui@gmail.com>
2026-03-23 12:56:13 -0300

    Product

---
+++ src/main/java/com/tonarqui/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.tonarqui.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tonarqui.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
