359d7e1c86afeaf7e25dbbe61765c2c95f0a7492
alexandretonarqui <alexandre.tonarqui@gmail.com>
2026-03-24 15:57:46 -0300

    OrderItem, many-to-many association with extra attributes PART 2

---
+++ src/main/java/com/tonarqui/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.tonarqui.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tonarqui.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}
